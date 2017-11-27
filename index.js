7d8cfc308fa0f146d7b01f249cb7b363ccee5117
DeepKolos <k1206521365k@126.com>
2017-11-27 08:53:16 +0800

    添加类型定义, 但是感觉十分需要TS的支援

--- index.js
+++ index.js
@@ -40,6 +40,86 @@ var _bubble_started = false;
 var _dom_involved;// [], order from bubble start to end
 var _last_dom_involved;
 
+
+const _TYPE_MONENT = 0;
+const _TYPE_CONTINUOUS = 1;
+const _TYPE_UNKNOW = 2;
+
+const _ON_FINGER = 0;
+const _ON_DOM = 1;
+const _ON_EVENT = 2;
+
+const _EVENT_TYPE = {
+  // 离散事件
+  tap: {
+    type: _TYPE_MONENT,
+    on: _ON_FINGER
+  },
+  longtap: {
+    type: _TYPE_MONENT,
+    on: _ON_FINGER
+  },
+
+  //连续事件
+  swipe: {
+    type: _TYPE_CONTINUOUS,
+    on: _ON_FINGER
+  },
+  pinch: {
+    type: _TYPE_CONTINUOUS,
+    on: _ON_FINGER
+  },
+  rotate: {
+    type: _TYPE_CONTINUOUS,
+    on: _ON_FINGER
+  },
+  //finger事件的自定义组合
+  group: {
+    type: _TYPE_UNKNOW,
+    on: _ON_FINGER
+  },
+
+  //相对dom来说
+  focus: {
+    type: _TYPE_MONENT,
+    on: _ON_DOM
+  },
+  blur: {
+    type: _TYPE_MONENT,
+    on: _ON_DOM
+  },
+  enter: {
+    type: _TYPE_MONENT,
+    on: _ON_DOM
+  },
+  leave: {
+    type: _TYPE_MONENT,
+    on: _ON_DOM
+  },
+  over: {
+    type: _TYPE_CONTINUOUS,
+    on: _ON_DOM
+  },
+
+  //对事件的继续分化事件
+  groupstart: {
+    type: _TYPE_MONENT,
+    on: _ON_EVENT
+  },
+  groupend: {
+    type: _TYPE_MONENT,
+    on: _ON_EVENT
+  },
+  bubblestart: {
+    type: _TYPE_MONENT,
+    on: _ON_EVENT
+  },
+  bubbleend: {
+    type: _TYPE_MONENT,
+    on: _ON_EVENT
+  }
+};
+
 function _touchstart (evt){
   _triggerbubble(this, evt);
 }
@@ -74,7 +154,7 @@ function _bubbleend(evt){
   }
 }
 
-function _groupstart(evt){
+function _groupstart(evt){1
   //初始化这次group涉及涉及的dom
   _dom_involved = [];
   evt.path.forEach(function($dom){
@@ -88,6 +168,34 @@ function _groupstart(evt){
   
   //生成schedule
   _dom_involved.forEach(function($dom){
+    var list = $dom.__event.list;
+    for(var type in list){
+      if(type === 'group'){
+
+        ;
+
+      }else{
+
+        list[type].forEach(function(config){
+
+          if(typeof config.repeat === 'number' && config.repeat > 1){
+            // 将会由group来实现
+
+          }else{
+            // 其实直接写入好了,不需要重复
+            _schedule.base[type] = {
+              status: 'init',
+              finger: 1
+            };
+            //这是离散/连续事件公有的
+            //但是连续事件会有多两个状态
+
+          }
+
+        });
+      }
+      
+    }
 
   });
 }
